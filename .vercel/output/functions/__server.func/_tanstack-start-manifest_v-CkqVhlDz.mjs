00bc53a52870273c9e8c7afa24375af55c0d26a4
piyush <piyushwaghjale26@gmail.com>
2026-07-13 00:38:48 +0530

    chore: replace lovable favicon with project logo

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CkqVhlDz.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CkqVhlDz.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/Users/piyush/Desktop/SecureLens/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-CCklxq-l.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CCklxq-l.js"
+		} }]
+	},
+	"/": {
+		filePath: "/Users/piyush/Desktop/SecureLens/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-CA68Y-S3.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
